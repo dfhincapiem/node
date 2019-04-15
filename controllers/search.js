dc72d67491ef7b105aea7087b74dc81c323f3ede
diegoh <product_developer1@arable.bkdf.nyc>
2019-04-14 19:43:20 -0500

    Fixed error

--- controllers/search.js
+++ controllers/search.js
@@ -6,9 +6,11 @@ const data = require ('./mockdata');
 
 router.get('/', function(req, res){
     if (req.query.name) {
-    res.send(data.filter(val=>val.name.toLowerCase().indexOf(req.query.name.toLowerCase()) > -1));
+    	res.send(data.filter(val=>val.name.toLowerCase().indexOf(req.query.name.toLowerCase()) > -1));
     }
-    res.send(data.filter(val=>val.stars === parseInt(req.query.stars)));
+    else {
+    	res.send(data.filter(val=>val.stars === parseInt(req.query.stars)));
+    }
   });
 
 module.exports = router
